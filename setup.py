71f472bd6eeb8bd14c59f38cbda5c2106353a9e1
Tomáš Ballák <tomas.ballak@protonmail.com>
2024-01-07 10:53:41 +0100

    0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='kernel-remove',
-    version='0.1',
+    version='0.2',
     packages=['kernel-remove'],
     install_requires=['inquirer'],
     entry_points={
